8b1c2249e532d1222f6cecf696a268338f1e9528
Vidar Wahlberg <canidae@exent.net>
2014-04-17 16:12:23 +0200

    persisting schedule on google app engine

--- appengine/src/net/exent/flywithme/ScheduleServlet.java
+++ appengine/src/net/exent/flywithme/ScheduleServlet.java
@@ -1,11 +1,13 @@
 package net.exent.flywithme;
 
+import com.google.appengine.api.datastore.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
@@ -16,10 +18,33 @@ public class ScheduleServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(ScheduleServlet.class.getName());
 
     // TODO: store registration time, store unregistered, have client ask for a time frame (registrations/unregistrations for the last x seconds)
-    private static Map<Integer, TakeoffSchedule> schedules = new HashMap<>();
+    private static final Key DATASTORE_SCHEDULES_KEY = KeyFactory.createKey("FlyWithMe", "Schedules");
+    private static final Map<Integer, TakeoffSchedule> schedules = new HashMap<>();
     private static long lastScheduleClean = System.currentTimeMillis();
 
-    /*
+    public ScheduleServlet() {
+        // read in schedule from datastore
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        try {
+            Entity entity = datastore.get(DATASTORE_SCHEDULES_KEY);
+            for (Map.Entry<String, Object> entry : entity.getProperties().entrySet()) {
+                try {
+                    Text text = (Text) entry.getValue();
+                    ByteArrayInputStream inputStream = new ByteArrayInputStream(text.getValue().getBytes());
+                    XMLDecoder decoder = new XMLDecoder(inputStream);
+                    schedules.put(Integer.valueOf(entry.getKey()), (TakeoffSchedule) decoder.readObject());
+                } catch (Exception e) {
+                    // failed reading in schedule? log error, and continue trying to read in remaining
+                    log.log(Level.WARNING, "Failed reading in schedule for takeoff " + entry.getKey() + ": " + entry.getValue(), e);
+                }
+            }
+        } catch (EntityNotFoundException e) {
+            // seems like our datastore doesn't have any schedules
+            log.log(Level.INFO, "No data found in datastore");
+        }
+    }
+
+    /* Used for testing, this code should not be uploaded to google app engine
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         // TODO: remove, just for testing
@@ -82,9 +107,10 @@ public class ScheduleServlet extends HttpServlet {
         con.setDoOutput(true);
         outputStream = new DataOutputStream(con.getOutputStream());
         outputStream.writeByte(0);
-        outputStream.writeShort(2);
+        outputStream.writeShort(3);
         outputStream.writeShort(4);
         outputStream.writeShort(67);
+        outputStream.writeShort(1337);
         outputStream.close();
         responseCode = con.getResponseCode();
         System.out.println("Response code: " + responseCode);
@@ -131,9 +157,10 @@ public class ScheduleServlet extends HttpServlet {
         con.setDoOutput(true);
         outputStream = new DataOutputStream(con.getOutputStream());
         outputStream.writeByte(0);
-        outputStream.writeShort(2);
+        outputStream.writeShort(3);
         outputStream.writeShort(4);
         outputStream.writeShort(67);
+        outputStream.writeShort(1337);
         outputStream.close();
         responseCode = con.getResponseCode();
         System.out.println("Response code: " + responseCode);
@@ -186,7 +213,7 @@ public class ScheduleServlet extends HttpServlet {
                     // long: pilotId
                     // string: name
                     // string: phone
-                    registerScheduleEntryV1(inputStream, outputStream);
+                    registerScheduleEntryV1(inputStream);
                     // output:
                     // <none>
                     break;
@@ -195,7 +222,7 @@ public class ScheduleServlet extends HttpServlet {
                     // input:
                     // ushort: takeoffId
                     // long: pilotId
-                    unregisterScheduleEntryV1(inputStream, outputStream);
+                    unregisterScheduleEntryV1(inputStream);
                     // output:
                     // <none>
                     break;
@@ -217,6 +244,7 @@ public class ScheduleServlet extends HttpServlet {
                 if (takeoffSchedule.isEmpty())
                     scheduleIterator.remove();
             }
+            storeSchedules();
             lastScheduleClean = currentTime;
         } catch (Exception e) {
             log.log(Level.WARNING, "Exception handling request", e);
@@ -231,17 +259,21 @@ public class ScheduleServlet extends HttpServlet {
 
         for (int i = 0; i < takeoffCount; ++i) {
             int takeoffId = inputStream.readUnsignedShort();
-            outputStream.writeShort(takeoffId);
             TakeoffSchedule takeoffSchedule = schedules.get(takeoffId);
-            Map<Long, Set<Pilot>> schedule = takeoffSchedule.getSchedule();
+            if (takeoffSchedule == null)
+                continue;
+            Map<Long, Set<Pilot>> schedule = takeoffSchedule.getEntries();
+            if (schedule.isEmpty())
+                continue;
+            outputStream.writeShort(takeoffId);
             outputStream.writeShort(schedule.size());
             for (Map.Entry<Long, Set<Pilot>> scheduleEntry : schedule.entrySet()) {
                 outputStream.writeLong(scheduleEntry.getKey());
                 Set<Pilot> pilots = scheduleEntry.getValue();
                 outputStream.writeShort(pilots.size());
                 for (Pilot pilot : pilots) {
-                    outputStream.writeUTF(pilot.name);
-                    outputStream.writeUTF(pilot.phone);
+                    outputStream.writeUTF(pilot.getName());
+                    outputStream.writeUTF(pilot.getPhone());
                 }
             }
         }
@@ -249,7 +281,7 @@ public class ScheduleServlet extends HttpServlet {
     }
 
     /* register a flight at a takeoff */
-    private static synchronized void registerScheduleEntryV1(final DataInputStream inputStream, final DataOutputStream outputStream) throws IOException {
+    private static synchronized void registerScheduleEntryV1(final DataInputStream inputStream) throws IOException {
         int takeoffId = inputStream.readUnsignedShort();
         long timestamp = inputStream.readLong();
         long pilotId = inputStream.readLong();
@@ -261,26 +293,71 @@ public class ScheduleServlet extends HttpServlet {
             schedules.put(takeoffId, takeoffSchedule);
         }
         takeoffSchedule.addPilotToSchedule(timestamp, new Pilot(pilotId, pilotName, pilotPhone));
+        storeSchedules();
     }
 
     /* unregister a flight at a takeoff */
-    private static synchronized void unregisterScheduleEntryV1(final DataInputStream inputStream, final DataOutputStream outputStream) throws IOException {
+    private static synchronized void unregisterScheduleEntryV1(final DataInputStream inputStream) throws IOException {
         int takeoffId = inputStream.readUnsignedShort();
         long timestamp = inputStream.readLong();
         long pilotId = inputStream.readLong();
         TakeoffSchedule takeoffSchedule = schedules.get(takeoffId);
         if (takeoffSchedule != null)
             takeoffSchedule.removePilotFromSchedule(timestamp, pilotId);
+        storeSchedules();
     }
 
-    private static class Pilot {
-        public long id;
-        public String name;
-        public String phone;
+    private static void storeSchedules() {
+        Entity entity = new Entity(DATASTORE_SCHEDULES_KEY);
+        for (Map.Entry<Integer, TakeoffSchedule> schedule : schedules.entrySet()) {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            XMLEncoder xmlEncoder = new XMLEncoder(bos);
+            xmlEncoder.writeObject(schedule.getValue());
+            xmlEncoder.close();
+            entity.setProperty(schedule.getKey().toString(), new Text(bos.toString()));
+        }
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(entity);
+    }
+
+    // NOTE! even though it may appear like the setters and getters are not in use, they are!
+    // deserializing from datastore use the default constructor and the setters to rebuild the object!
+    public static class Pilot {
+        private long id;
+        private String name;
+        private String phone;
+
+        public Pilot() {
+            // required for deserialization
+        }
 
         public Pilot(long id, String name, String phone) {
             this.id = id;
             this.name = name;
+            this.phone = phone;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
             this.phone = phone;
         }
 
@@ -295,7 +372,9 @@ public class ScheduleServlet extends HttpServlet {
         }
     }
 
-    private static class TakeoffSchedule {
+    // NOTE! even though it may appear like the setters and getters are not in use, they are!
+    // deserializing from datastore use the default constructor and the setters to rebuild the object!
+    public static class TakeoffSchedule {
         private Map<Long, Set<Pilot>> entries = new HashMap<>();
 
         public void addPilotToSchedule(long timestamp, Pilot pilot) {
@@ -329,8 +408,12 @@ public class ScheduleServlet extends HttpServlet {
             return entries.isEmpty();
         }
 
-        public Map<Long, Set<Pilot>> getSchedule() {
+        public Map<Long, Set<Pilot>> getEntries() {
             return entries;
+        }
+
+        public void setEntries(Map<Long, Set<Pilot>> entries) {
+            this.entries = entries;
         }
     }
 }
