5e130e0b2695b569cd592efa00b288199be48b27
Vidar Wahlberg <canidae@exent.net>
2014-06-15 21:18:46 +0200

    added "share"-button for meteogram/sounding

--- android/flywithme/src/main/java/net/exent/flywithme/layout/NoaaForecast.java
+++ android/flywithme/src/main/java/net/exent/flywithme/layout/NoaaForecast.java
@@ -1,9 +1,14 @@
 package net.exent.flywithme.layout;
 
+import net.exent.flywithme.FlyWithMe;
 import net.exent.flywithme.R;
 import net.exent.flywithme.bean.Takeoff;
 import net.exent.flywithme.view.GestureImageView;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,13 +21,30 @@ public class NoaaForecast extends Fragment {
     public static final String ARG_TAKEOFF = "takeoff";
     private Takeoff takeoff;
 
-    public void showNoaaForecast(Takeoff takeoff) {
+    public void showNoaaForecast(final Takeoff takeoff) {
         try {
             this.takeoff = takeoff;
             TextView noaaForecastText = (TextView) getActivity().findViewById(R.id.noaaForecastText);
             noaaForecastText.setText(takeoff.getName());
             GestureImageView noaaForecastImage = (GestureImageView) getActivity().findViewById(R.id.noaaForecastImage);
             noaaForecastImage.setBitmap(takeoff.getNoaaforecast());
+
+            ImageButton navigationButton = (ImageButton) getActivity().findViewById(R.id.fragmentButton1);
+            navigationButton.setImageResource(R.drawable.share);
+            navigationButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    String title = takeoff.getName() + " (" + takeoff.getLocation().getLatitude() + ", " + takeoff.getLocation().getLongitude() + ")";
+                    String url = MediaStore.Images.Media.insertImage(FlyWithMe.getInstance().getContentResolver(), takeoff.getNoaaforecast(), title, null);
+
+                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                    sharingIntent.setType("image/*");
+                    sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(url));
+                    sharingIntent.putExtra(Intent.EXTRA_TEXT, title);
+                    startActivity(Intent.createChooser(sharingIntent, null));
+                }
+            });
+            ((ImageButton) getActivity().findViewById(R.id.fragmentButton2)).setImageDrawable(null);
+            ((ImageButton) getActivity().findViewById(R.id.fragmentButton3)).setImageDrawable(null);
         } catch (Exception e) {
             Log.w(getClass().getName(), "showNoaaForecast() failed unexpectedly", e);
         }
@@ -42,10 +64,6 @@ public class NoaaForecast extends Fragment {
         Bundle args = getArguments();
         if (args != null)
             showNoaaForecast((Takeoff) args.getParcelable(ARG_TAKEOFF));
-
-        ((ImageButton) getActivity().findViewById(R.id.fragmentButton1)).setImageDrawable(null);
-        ((ImageButton) getActivity().findViewById(R.id.fragmentButton2)).setImageDrawable(null);
-        ((ImageButton) getActivity().findViewById(R.id.fragmentButton3)).setImageDrawable(null);
     }
 
     @Override
