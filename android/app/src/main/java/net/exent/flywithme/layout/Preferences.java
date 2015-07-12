8b888fbc971aa7541642f707b28ffb4711cd67be
Vidar Wahlberg <canidae@exent.net>
2015-07-12 21:00:31 +0200

    removing old unused preferences, another index added

--- android/app/src/main/java/net/exent/flywithme/layout/Preferences.java
+++ android/app/src/main/java/net/exent/flywithme/layout/Preferences.java
@@ -2,15 +2,12 @@ package net.exent.flywithme.layout;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
-import java.util.TimeZone;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
@@ -31,23 +28,14 @@ public class Preferences extends PreferenceFragment implements SharedPreferences
     };
 
     public static void setupDefaultPreferences(Context context) {
-        // setup default sounding, two days in advantage, sounding nearest 12 local time (ignoring DST, which is an idiotic practice)
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        TimeZone tz = TimeZone.getDefault();
-        int tzOffsetIgnoringDst = (tz.getOffset(System.currentTimeMillis()) - (tz.inDaylightTime(new Date()) ? tz.getDSTSavings() : 0)) / 3600000;
-        for (int i = 0; i <= 21; i += 3) {
-            String hour = (i < 10 ? "0" + i : "" + i);
-            if (!prefs.contains("pref_sounding_at_" + hour))
-                prefs.edit().putBoolean("pref_sounding_at_" + hour, Math.abs(12 - tzOffsetIgnoringDst - i) <= 1).commit();
-        }
-
         // setup default airspace map polygons (all shown)
         ArrayList<String> airspaceList = new ArrayList<>(Airspace.getAirspaceMap().keySet());
         for (String key : airspaceList) {
             if (key == null || "".equals(key.trim()))
                 continue;
             if (!prefs.contains("pref_airspace_enabled_" + key))
-                prefs.edit().putBoolean("pref_airspace_enabled_" + key, true).commit();
+                prefs.edit().putBoolean("pref_airspace_enabled_" + key, true).apply();
         }
     }
 
@@ -58,15 +46,6 @@ public class Preferences extends PreferenceFragment implements SharedPreferences
         addPreferencesFromResource(R.xml.preferences);
 
         updateDynamicPreferenceScreen();
-
-        PreferenceCategory meteogramAndSounding = (PreferenceCategory) findPreference("pref_meteogram_and_sounding");
-        for (int i = 0; i <= 21; i += 3) {
-            String hour = (i < 10 ? "0" + i : "" + i);
-            CheckBoxPreference checkBoxPreference = new CheckBoxPreference(getActivity());
-            checkBoxPreference.setKey("pref_sounding_at_" + hour);
-            checkBoxPreference.setTitle("Fetch sounding at " + hour + " UTC");
-            meteogramAndSounding.addPreference(checkBoxPreference);
-        }
 
         PreferenceCategory showAirspaceTypesCategory = (PreferenceCategory) findPreference("pref_show_airspace_types");
         ArrayList<String> airspaceList = new ArrayList<>(Airspace.getAirspaceMap().keySet());
@@ -113,37 +92,11 @@ public class Preferences extends PreferenceFragment implements SharedPreferences
     /* END AAH! */
 
     private void updateDynamicPreferenceScreen() {
-        ListPreference scheduleFetchTakeoffs = (ListPreference) findPreference("pref_schedule_fetch_takeoffs");
-        if (scheduleFetchTakeoffs == null) {
-            // this shouldn't really happen, but apparently it does
-            // for now, just return and hope that the method will be called again soon enough
-            Log.w(getClass().getName(), "Unable to find preference widgets, can't update preference screen");
-            return;
-        }
-        scheduleFetchTakeoffs.setOnPreferenceChangeListener(preferenceChangeListener);
-        scheduleFetchTakeoffs.setSummary(scheduleFetchTakeoffs.getEntry());
-
-        PreferenceCategory scheduleCategory = (PreferenceCategory) findPreference("pref_schedule");
-        scheduleCategory.setEnabled(!"-1".equals(scheduleFetchTakeoffs.getValue()));
-
         EditTextPreference schedulePilotName = (EditTextPreference) findPreference("pref_schedule_pilot_name");
         schedulePilotName.setSummary(schedulePilotName.getText());
         EditTextPreference schedulePilotPhone = (EditTextPreference) findPreference("pref_schedule_pilot_phone");
         schedulePilotPhone.setSummary(schedulePilotPhone.getText());
         CheckBoxPreference scheduleNotification = (CheckBoxPreference) findPreference("pref_schedule_notification");
         scheduleNotification.setOnPreferenceChangeListener(preferenceChangeListener);
-        ListPreference scheduleFetchInterval = (ListPreference) findPreference("pref_schedule_update_interval");
-        scheduleFetchInterval.setOnPreferenceChangeListener(preferenceChangeListener);
-        scheduleFetchInterval.setSummary(scheduleFetchInterval.getEntry());
-        ListPreference scheduleFetchStartTime = (ListPreference) findPreference("pref_schedule_start_fetch_time");
-        scheduleFetchStartTime.setOnPreferenceChangeListener(preferenceChangeListener);
-        scheduleFetchStartTime.setSummary(scheduleFetchStartTime.getEntry());
-        ListPreference scheduleFetchStopTime = (ListPreference) findPreference("pref_schedule_stop_fetch_time");
-        scheduleFetchStopTime.setOnPreferenceChangeListener(preferenceChangeListener);
-        scheduleFetchStopTime.setSummary(scheduleFetchStopTime.getEntry());
-
-        ListPreference prefSoundingDays = (ListPreference) findPreference("pref_sounding_days");
-        prefSoundingDays.setOnPreferenceChangeListener(preferenceChangeListener);
-        prefSoundingDays.setSummary(prefSoundingDays.getEntry());
     }
 }
